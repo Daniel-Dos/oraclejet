18b93ff5524f3000d782360a830ffd6c5a447eb8
daniel-dos <daniel.dias.analistati@gmail.com>
2018-05-16 17:30:17 -0300

    update CRUD

--- src/js/viewModels/editUsuario.js
+++ src/js/viewModels/editUsuario.js
@@ -5,16 +5,16 @@
 /*
  * Your customer ViewModel code goes here
  */
-define(['ojs/ojcore', 'knockout', 'factories/customerController', 'jquery','sweetalert', 'ojs/ojmodel', 'ojs/ojbutton',
+define(['ojs/ojcore', 'knockout', 'factories/customerController', 'jquery', 'sweetalert', 'ojs/ojmodel', 'ojs/ojbutton',
     'ojs/ojinputtext', 'ojs/ojinputnumber'],
-        function (oj, ko, customerController,$,swal) {
+        function (oj, ko, customerController, $, swal) {
 
             function EditCustomerViewModel() {
+
                 var self = this;
                 self.customerModel = ko.observable();
                 self.update = customerController.editCustomerModel();
 
-                self.saveBtnBusyCheck = ko.observable(false);
 
                 self.initialize = function (params) {
                     var customerId = oj.Router.rootInstance.retrieve();
@@ -38,107 +38,89 @@ define(['ojs/ojcore', 'knockout', 'factories/customerController', 'jquery','swee
                 };
 
                 saveCustomer = function () {
-                    var btnSave = document.querySelector("#btnSave");
-                    var busyContext = oj.Context.getContext(btnSave).getBusyContext();
-
-                    if (busyContext.isReady()) {
-                        var options = {"description": "#Calling asynchronous REST endpoint"};
-                        var resolve = busyContext.addBusyState(options);
-                        self.saveBtnBusyCheck(!busyContext.isReady());
-                        
-                        self.update.save(self.customerModel().toJSON(), {
-                            contentType: 'application/json',
-                           
-                            success: function (response,data) {
-
-                                resolve();
-                                
-                                 swal(data.mensagem,"", "success");
-                                
-                                 oj.Router.rootInstance.store(-1);
-                                 oj.Router.rootInstance.go("tabela");
-                                 
-
-                                busyContext.whenReady().then(function () {
-                                    self.saveBtnBusyCheck(!busyContext.isReady());
-                                });
-                                
-                            },
-                            error: function (jqXHR, textStatus, errorThrown) {
-                                console.log("Update error: ", errorThrown);
-                                
-                                resolve();
-
-                                busyContext.whenReady().then(function () {
-                                    self.saveBtnBusyCheck(!busyContext.isReady());
-                                });
-                            }
-                        });
-                    }
-                };
-
-                // Below are a subset of the ViewModel methods invoked by the ojModule binding
-                // Please reference the ojModule jsDoc for additionaly available methods.
-
-                /**
-                 * Optional ViewModel method invoked when this ViewModel is about to be
-                 * used for the View transition.  The application can put data fetch logic
-                 * here that can return a Promise which will delay the handleAttached function
-                 * call below until the Promise is resolved.
-                 * @param {Object} info - An object with the following key-value pairs:
-                 * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-                 * @param {Function} info.valueAccessor - The binding's value accessor.
-                 * @return {Promise|undefined} - If the callback returns a Promise, the next phase (attaching DOM) will be delayed until
-                 * the promise is resolved
-                 */
-                self.handleActivated = function (info) {
-                    // Implement if needed
-                };
-
-                /**
-                 * Optional ViewModel method invoked after the View is inserted into the
-                 * document DOM.  The application can put logic that requires the DOM being
-                 * attached here.
-                 * @param {Object} info - An object with the following key-value pairs:
-                 * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-                 * @param {Function} info.valueAccessor - The binding's value accessor.
-                 * @param {boolean} info.fromCache - A boolean indicating whether the module was retrieved from cache.
-                 */
-                self.handleAttached = function (info) {
-                    // Implement if needed
-                };
 
 
-                /**
-                 * Optional ViewModel method invoked after the bindings are applied on this View. 
-                 * If the current View is retrieved from cache, the bindings will not be re-applied
-                 * and this callback will not be invoked.
-                 * @param {Object} info - An object with the following key-value pairs:
-                 * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-                 * @param {Function} info.valueAccessor - The binding's value accessor.
-                 */
-                self.handleBindingsApplied = function (info) {
-                    // Implement if needed
-                };
 
-                /*
-                 * Optional ViewModel method invoked after the View is removed from the
-                 * document DOM.
-                 * @param {Object} info - An object with the following key-value pairs:
-                 * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-                 * @param {Function} info.valueAccessor - The binding's value accessor.
-                 * @param {Array} info.cachedNodes - An Array containing cached nodes for the View if the cache is enabled.
-                 */
-                self.handleDetached = function (info) {
-                    // Implement if needed
-                };
+
+                    self.update.save(self.customerModel().toJSON(), {
+                        contentType: 'application/json',
+
+                        success: function (response, data) {
+
+
+
+                            swal(data.mensagem, "", "success");
+
+                            oj.Router.rootInstance.store(-1);
+                            oj.Router.rootInstance.go("tabela");
+
+
+
+                        },
+                        error: function (jqXHR, textStatus, errorThrown) {
+                            console.log("Update error: ", errorThrown);
+                        }
+                    });
+                }
             }
+            ;
+
+            // Below are a subset of the ViewModel methods invoked by the ojModule binding
+            // Please reference the ojModule jsDoc for additionaly available methods.
+
+            /**
+             * Optional ViewModel method invoked when this ViewModel is about to be
+             * used for the View transition.  The application can put data fetch logic
+             * here that can return a Promise which will delay the handleAttached function
+             * call below until the Promise is resolved.
+             * @param {Object} info - An object with the following key-value pairs:
+             * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
+             * @param {Function} info.valueAccessor - The binding's value accessor.
+             * @return {Promise|undefined} - If the callback returns a Promise, the next phase (attaching DOM) will be delayed until
+             * the promise is resolved
+             */
+            self.handleActivated = function (info) {
+                // Implement if needed
+            };
+
+            /**
+             * Optional ViewModel method invoked after the View is inserted into the
+             * document DOM.  The application can put logic that requires the DOM being
+             * attached here.
+             * @param {Object} info - An object with the following key-value pairs:
+             * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
+             * @param {Function} info.valueAccessor - The binding's value accessor.
+             * @param {boolean} info.fromCache - A boolean indicating whether the module was retrieved from cache.
+             */
+            self.handleAttached = function (info) {
+                // Implement if needed
+            };
+
+
+            /**
+             * Optional ViewModel method invoked after the bindings are applied on this View. 
+             * If the current View is retrieved from cache, the bindings will not be re-applied
+             * and this callback will not be invoked.
+             * @param {Object} info - An object with the following key-value pairs:
+             * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
+             * @param {Function} info.valueAccessor - The binding's value accessor.
+             */
+            self.handleBindingsApplied = function (info) {
+                // Implement if needed
+            };
 
             /*
-             * Returns a constructor for the ViewModel so that the ViewModel is constrcuted
-             * each time the view is displayed.  Return an instance of the ViewModel if
-             * only one instance of the ViewModel is needed.
+             * Optional ViewModel method invoked after the View is removed from the
+             * document DOM.
+             * @param {Object} info - An object with the following key-value pairs:
+             * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
+             * @param {Function} info.valueAccessor - The binding's value accessor.
+             * @param {Array} info.cachedNodes - An Array containing cached nodes for the View if the cache is enabled.
              */
+            self.handleDetached = function (info) {
+                // Implement if needed
+            };
+
             return new EditCustomerViewModel();
         }
 );
